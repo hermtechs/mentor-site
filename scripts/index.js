e2f36f3d3b777e57932b167ec6f2744543421b0b
hermtechs <hermtechs@gmail.com>
2023-07-03 19:51:02 +0300

    added an embedded video popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+"use strict";
 //navingation
 const navbars = document.querySelector(".nav-bars");
 const smallLinksContainer = document.querySelector(".side-menu-links");
@@ -19,3 +20,34 @@ sideMenulinks.forEach((link) =>
     smallLinksContainer.classList.add("show-mobile-menu");
   })
 );
+
+//video popup jquery code
+ $(document).ready(function() {
+
+// Gets the video src from the data-src on each button
+
+var $videoSrc;  
+$('.video-btn').click(function() {
+    $videoSrc = $(this).data( "src" );
+});
+console.log($videoSrc);
+  
+// when the modal is opened autoplay it  
+$('#myModal').on('shown.bs.modal', function (e) {
+    
+// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+
+})
+  
+
+
+// stop playing the youtube video when I close the modal
+$('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src',$videoSrc); 
+}) 
+    
+
+// document ready  
+});
