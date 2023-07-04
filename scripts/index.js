04fd87213b71c3db5effe462a87e6c9b177fc867
hermtechs <hermtechs@gmail.com>
2023-07-04 23:08:46 +0300

    added functionality for all Get Started button

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,9 @@
 const navbars = document.querySelector(".nav-bars");
 const smallLinksContainer = document.querySelector(".side-menu-links");
 const sideMenulinks = document.querySelectorAll(".side-links a");
-const closeOverlayBtn = document.querySelector('.close-overlay');
-const overlay = document.querySelector('.contact-us-overlay')
+const closeOverlayBtn = document.querySelector(".close-overlay");
+const overlay = document.querySelector(".contact-us-overlay");
+const openContactUsOverlayBtn = document.querySelectorAll("#open-contact-us");
 
 navbars.addEventListener("click", () => {
   if (navbars.classList.contains("rotate")) {
@@ -24,37 +25,47 @@ sideMenulinks.forEach((link) =>
 );
 
 //video popup jquery code
- $(document).ready(function() {
+$(document).ready(function () {
+  // Gets the video src from the data-src on each button
 
-// Gets the video src from the data-src on each button
+  var $videoSrc;
+  $(".video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
 
-var $videoSrc;  
-$('.video-btn').click(function() {
-    $videoSrc = $(this).data( "src" );
-});
-console.log($videoSrc);
-  
-// when the modal is opened autoplay it  
-$('#myModal').on('shown.bs.modal', function (e) {
-    
-// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+  // when the modal is opened autoplay it
+  $("#myModal").on("shown.bs.modal", function (e) {
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+    );
+  });
 
-})
-  
-
-
-// stop playing the youtube video when I close the modal
-$('#myModal').on('hide.bs.modal', function (e) {
+  // stop playing the youtube video when I close the modal
+  $("#myModal").on("hide.bs.modal", function (e) {
     // a poor man's stop video
-    $("#video").attr('src',$videoSrc); 
-}) 
-    
+    $("#video").attr("src", $videoSrc);
+  });
 
-// document ready  
+  // document ready
 });
 
 //close overlay
-closeOverlayBtn.addEventListener('click', ()=>{
-  overlay.style.transform = "translateY(100%)"
-})
+closeOverlayBtn.addEventListener("click", () => {
+  overlay.style.transform = "translateY(100%)";
+});
+
+openContactUsOverlayBtn.forEach((btn) => {
+  btn.addEventListener("click", openAndCloseOverlay);
+});
+
+function openAndCloseOverlay() {
+  overlay.style.transition = "320ms ease-in-out";
+  overlay.style.transform = "translateY(100%)";
+
+  overlay.style.transform === "translateY(100%)"
+    ? (overlay.style.transform = "translateY(0%)")
+    : (overlay.style.transform = "translateY(100%)");
+}
